fed3abf4a74ed6cd3617869277b04c503d4dd107
Dendup Tshering <164316548+Dendup1234@users.noreply.github.com>
2025-04-20 02:40:21 +0600

    Added the create

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root "articles#index"
   get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 
   get "up" => "rails/health#show", as: :rails_health_check
 
