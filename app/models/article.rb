e21141aa34eadd8b754f836c120fd35609141254
Dendup Tshering <164316548+Dendup1234@users.noreply.github.com>
2025-04-20 20:16:47 +0600

    CRUD and model added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
+    #For creating one to many relationship
+    include Visible
+
+    has_many :comments, dependent: :destroy
+
     validates :title, presence: true
     validates :body, presence: true, length: {minimum: 10}
+
 end
